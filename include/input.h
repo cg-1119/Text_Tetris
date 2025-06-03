54c7dc0c3560a04c8b4602b9b5f4d5c9886551d9
copperminesp <son5121502@gmail.com>
2025-06-03 21:59:49 +0900

    chore: remove cmake and restruct directories

--- include/input.h
+++
@@ -1,8 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-void setup_terminal(void);
-void restore_terminal(void);
-int get_key(void);
-
-#endif //INPUT_H
--- include/render.h
+++
@@ -1,8 +0,0 @@
-#ifndef TETRIS_RENDER_H
-#define TETRIS_RENDER_H
-
-void draw_screen(void);
-void draw_board(void);
-void draw_tetromino(void);
-
-#endif // TETRIS_RENDER_H
