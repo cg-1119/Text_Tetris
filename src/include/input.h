fc3ae31f203826a9f20a4e7a53dd5f8a9e0092cf
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2025-06-04 20:42:15 +0900

    chore: 함수 이니셜라이저에 설명 이동

--- src/include/input.h
+++ src/include/input.h
@@ -1,7 +1,13 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+/**
+ * 터미널을 논블로킹·논에코 모드로 전환하고, 화면 출력 시 커서를 숨깁니다.
+ */
 void setup_terminal(void);
+/**
+ * 터미널 설정을 원래대로 복구하고, 커서를 다시 보이게 합니다.
+ */
 void restore_terminal(void);
 int get_key(void);
 
--- src/input.c
+++ src/input.c
@@ -3,12 +3,9 @@
 #include <termios.h>
 #include <unistd.h>
 
-/* 이전 터미널 설정을 저장할 변수 */
+// 이전 터미널 설정을 저장할 변수
 static struct termios old_tio, new_tio;
 
-/**
- * 터미널을 논블로킹·논에코 모드로 전환하고, 화면 출력 시 커서를 숨깁니다.
- */
 void setup_terminal(void) {
     tcgetattr(STDIN_FILENO, &old_tio);
     new_tio = old_tio; // 원래 설정 old_tio에 저장
@@ -23,10 +20,6 @@ void setup_terminal(void) {
     printf("\x1b[?25l"); // ANSI 이스케이프 코드로 커서 숨기기
     fflush(stdout);
 }
-
-/**
- * 터미널 설정을 원래대로 복구하고, 커서를 다시 보이게 합니다.
- */
 void restore_terminal(void) {
     tcsetattr(STDIN_FILENO, TCSANOW, &old_tio); // 원래 설정으로 복원
     printf("\x1b[?25h"); // ANSI Escape 코드로 커서 보이게 수정
